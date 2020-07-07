22794bd47f626475da56ed9bdd47e48f78b0592a
mrwdhasd0903 <2553378438@qq.com>
2020-07-08 00:24:48 +0800

    修改配置文件

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Button from '../packages/Button'
+import Icon from '../packages/Icon'
+
+export {
+  Button,
+  Icon
+}
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 禁用eslint-loader
-  lintOnSave:false,
+  lintOnSave: false,
   pages: {
     index: {
       entry: 'examples/main.js',
@@ -8,14 +8,21 @@ module.exports = {
       filename: 'index.html'
     }
   },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        vue$: "vue/dist/vue.esm.js", //加上这一句
+      }
+    }
+  },
   // 扩展 webpack 配置，使 packages 加入编译
   chainWebpack: config => {
     config.module
       .rule('js')
       .include
-        .add('/packages')
-        .end()
+      .add('/packages')
+      .end()
       .use('babel')
-        .loader('babel-loader')
+      .loader('babel-loader')
   }
 }
