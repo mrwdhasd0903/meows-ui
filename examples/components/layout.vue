1e967312c65d1f0d27800a548d4e2693585509fe
mrwdhasd0903 <2553378438@qq.com>
2020-07-19 00:43:52 +0800

    完成响应式(媒体查询)

--- examples/components/layout.vue
+++ examples/components/layout.vue
@@ -4,7 +4,7 @@
       <me-col span="1" v-for="(item, index) in arr" :key="index">{{index}}</me-col>
     </me-row>
     <me-row space="20">
-      <me-col span="8">
+      <me-col span="8" xs="0" sm="8" md="6" lg="4">
         <div class="tt">123</div>
       </me-col>
       <me-col span="8">
@@ -18,7 +18,7 @@
       </me-col>
     </me-row>
     <me-row>
-      <me-col offset="8" span="8">
+      <me-col offset="8" offset-xs="2" offset-sm="4" offset-md="6" offset-lg="10" span="8">
         <div class="tt">123</div>
       </me-col>
       <me-col span="6">
--- packages/Layout/src/col.vue
+++ packages/Layout/src/col.vue
@@ -3,6 +3,7 @@
     class="me-col"
     :class="[
     'me-span--'+span,
+    ...mediaClass,
     offset?'me-offset--'+offset:''
   ]"
     :style="{
@@ -22,7 +23,15 @@ export default {
     },
     offset: {
       default: 0
-    }
+    },
+    xs: [Number, String],
+    sm: [Number, String],
+    md: [Number, String],
+    lg: [Number, String],
+    offsetXs: [Number, String],
+    offsetSm: [Number, String],
+    offsetMd: [Number, String],
+    offsetLg: [Number, String]
   },
   computed: {
     //计算padding
@@ -36,6 +45,21 @@ export default {
         paddingLeft: padding + "px",
         paddingRight: padding + "px"
       };
+    },
+    mediaClass() {
+      let classList = [];
+      ["xs", "sm", "md", "lg"].forEach(item => {
+        if (this[item] || this[item] === 0) {
+          classList.push("me-span-" + item + "--" + this[item]);
+        }
+      });
+      ["offsetXs", "offsetSm", "offsetMd", "offsetLg"].forEach(item => {
+        if (this[item]) {
+          let size = item.substring(6).toLocaleLowerCase();
+          classList.push("me-offset-" + size + "--" + this[item]);
+        }
+      });
+      return classList;
     }
   }
 };
@@ -45,6 +69,7 @@ export default {
 .me-col {
   @import "./colspan.scss";
   display: inline-block;
+  transition: width 0.2s ease, margin-left 0.2s ease;
 }
 @import "./colspan-xs.scss";
 @import "./colspan-sm.scss";
