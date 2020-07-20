50fb8b783c475e50af2b3ac4153e2d438fe868bc
mrwdhasd0903 <2553378438@qq.com>
2020-07-21 02:05:17 +0800

    完成基于api的3d旋转

--- examples/components/three.vue
+++ examples/components/three.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dddiv">
-    <me-three>
+    <me-three ref="three" width="120" height="160" deep="200">
       <me-surface slot="front">前</me-surface>
       <me-surface slot="back">后</me-surface>
       <me-surface slot="top">上</me-surface>
@@ -8,7 +8,12 @@
       <me-surface slot="left">左</me-surface>
       <me-surface slot="right">右</me-surface>
     </me-three>
-    <!-- <me-button>按钮</me-button> -->
+    <me-button style="marginLeft:300px" @click="rollBack('top')">↑</me-button>
+    <me-button style="marginLeft:300px;marginTop:40px" @click="rollBack('bottom')">↓</me-button>
+    <me-button style="marginLeft:240px;marginTop:-130px" @click="rollBack('left')">←</me-button>
+    <me-button style="marginLeft:350px;marginTop:-160px" @click="rollBack('right')">→</me-button>
+    <input style="marginLeft:350px;marginTop:-160px" type="text" v-model="code" />
+    <me-button style="marginLeft:350px;marginTop:-160px" @click="evalString">执行</me-button>
   </div>
 </template>
 
@@ -17,10 +22,27 @@ export default {
   name: "",
   components: {},
   data() {
-    return {};
+    return {
+      code: ""
+    };
   },
   computed: {},
-  methods: {},
+  methods: {
+    evalString() {
+      var arr = this.code.split(" ");
+      this.$refs.three.evalString(
+        "this.rotateX=" +
+          arr[0] +
+          ";this.rotateY=" +
+          arr[1] +
+          ";this.rotateZ=" +
+          arr[2]
+      );
+    },
+    rollBack(flag) {
+      this.$refs.three.rollBack(flag);
+    }
+  },
   mounted() {}
 };
 </script>
--- packages/Three/src/three.vue
+++ packages/Three/src/three.vue
@@ -1,21 +1,69 @@
 <template>
-  <div class="me-three">
-    <div class="me-three-front">
+  <div
+    class="me-three"
+    :style="{
+  transform: 'perspective('+perspective+'px) rotateX('+rotateX+'deg) rotateY('+rotateY+'deg) rotateZ('+rotateZ+'deg)',
+  transformOrigin: width/2+'px '+height/2+'px '+(-deep)/2+'px',
+  perspectiveOrigin: width/2+'px '+height/2+'px'
+  }"
+  >
+    <div
+      class="me-three-front"
+      :style="{
+      width: width+'px',
+      height: height+'px',
+    }"
+    >
       <slot name="front"></slot>
     </div>
-    <div class="me-three-back">
+    <div
+      class="me-three-back"
+      :style="{
+      width:width+'px',
+      height:height+'px',
+      transform: 'rotateY(-180deg) translateZ('+deep+'px)'
+    }"
+    >
       <slot name="back"></slot>
     </div>
-    <div class="me-three-top">
+    <div
+      class="me-three-top"
+      :style="{
+      width: width+'px',
+      height: deep+'px',
+      transform: 'rotateX(90deg) translateZ('+deep/2+'px) translateY('+(-deep/2)+'px)'
+    }"
+    >
       <slot name="top"></slot>
     </div>
-    <div class="me-three-bottom">
+    <div
+      class="me-three-bottom"
+      :style="{
+      width: width+'px',
+      height: deep+'px',
+      transform: 'rotateX(-90deg) translateZ('+(height - deep/2)+'px) translateY('+(deep/2)+'px)'
+    }"
+    >
       <slot name="bottom"></slot>
     </div>
-    <div class="me-three-left">
+    <div
+      class="me-three-left"
+      :style="{
+      width: deep+'px',
+      height: height+'px',
+      transform: 'rotateY(-90deg) translateZ('+(deep/2)+'px) translateX('+(-deep/2)+'px)'
+    }"
+    >
       <slot name="left"></slot>
     </div>
-    <div class="me-three-right">
+    <div
+      class="me-three-right"
+      :style="{
+      width:deep+'px',
+      height: height+'px',
+      transform: 'rotateY(90deg) translateZ('+(width - deep/2)+'px) translateX('+(deep/2)+'px)'
+    }"
+    >
       <slot name="right"></slot>
     </div>
   </div>
@@ -26,11 +74,73 @@ export default {
   name: "MeThree",
   components: {},
   data() {
-    return {};
+    return {
+      perspective: 1000,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0
+    };
+  },
+  props: {
+    width: {
+      default: 100
+    },
+    height: {
+      default: 100
+    },
+    deep: {
+      default: 100
+    }
   },
   computed: {},
-  methods: {},
-  mounted() {}
+  methods: {
+    evalString(code) {
+    console.log(this.rotateX, this.rotateY, this.rotateZ);
+      eval(code);
+    },
+    rollBack(flag) {
+      var _rotateX = this.rotateX;
+      while (_rotateX < 0) {
+        _rotateX = _rotateX + 360;
+      }
+      switch (flag) {
+        case "left":
+          if (_rotateX % 360 == 0) {
+            this.rotateY = this.rotateY + 90;
+          } else if (_rotateX % 270 == 0) {
+            this.rotateZ = this.rotateZ + 90;
+          } else if (_rotateX % 180 == 0) {
+            this.rotateY = this.rotateY - 90;
+          } else if (_rotateX % 90 == 0) {
+            this.rotateZ = this.rotateZ - 90;
+          }
+          break;
+        case "right":
+          if (_rotateX % 360 == 0) {
+            this.rotateY = this.rotateY - 90;
+          } else if (_rotateX % 270 == 0) {
+            this.rotateZ = this.rotateZ - 90;
+          } else if (_rotateX % 180 == 0) {
+            this.rotateY = this.rotateY + 90;
+          } else if (_rotateX % 90 == 0) {
+            this.rotateZ = this.rotateZ + 90;
+          }
+          break;
+        case "top":
+          this.rotateX = this.rotateX - 90;
+          break;
+        case "bottom":
+          this.rotateX = this.rotateX + 90;
+          break;
+        default:
+          console.error("错误参数,请选择:left/right/top/bottom");
+      }
+      console.log(this.rotateX, this.rotateY, this.rotateZ);
+    }
+  },
+  mounted() {
+    console.log(this.rotateX, this.rotateY, this.rotateZ);
+  }
 };
 </script>
 
@@ -41,9 +151,7 @@ export default {
   $deep: 200px;
   display: inline-block;
   transform-style: preserve-3d;
-  transform:perspective(1000px) rotateX(0deg) rotateY(0deg) rotateZ(0deg);
-  transform-origin: $width/2 $height/2 0px;
-  perspective-origin: 60px 80px;
+  transition: transform 0.3s ease;
   & > div {
     display: flex;
     align-items: center;
@@ -54,39 +162,21 @@ export default {
   }
   & > .me-three-top {
     background: rgba(210, 210, 0, 0.7);
-    width: $width;
-    height: $deep;
-    transform: rotateX(90deg) translateZ($deep/2);
   }
   & > .me-three-bottom {
     background: rgba(210, 0, 210, 0.7);
-    width: $width;
-    height: $deep;
-    transform: rotateX(-90deg) translateZ($height - $deep/2);
   }
   & > .me-three-left {
     background: rgba(0, 0, 210, 0.7);
-    width: $deep;
-    height: $height;
-    transform: rotateY(-90deg) translateZ($deep/2);
   }
   & > .me-three-right {
     background: rgba(210, 0, 0, 0.7);
-    width: $deep;
-    height: $height;
-    transform: rotateY(90deg) translateZ($width - $deep/2);
   }
   & > .me-three-front {
     background: rgba(90, 90, 90, 0.7);
-    width: $width;
-    height: $height;
-    transform: translateZ($deep/2);
   }
   & > .me-three-back {
     background: rgba(0, 210, 0, 0.7);
-    width: $width;
-    height: $height;
-    transform: rotateY(-180deg) translateZ($deep/2);
   }
 }
 </style>
