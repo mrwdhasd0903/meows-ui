339a4b0cdc00e8a5d3dd12987e705c8f77372d96
mrwdhasd0903 <2553378438@qq.com>
2020-08-05 16:00:18 +0800

    完成switch样式设计

--- packages/Switch/src/index.vue
+++ packages/Switch/src/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class>switch</div>
+  <div class="me-switch">
+    <span class="me-switch__core">
+      <span class="me-switch__button"></span>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -15,5 +19,39 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss">
+.me-switch {
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+  font-size: 14px;
+  line-height: 20px;
+  height: 20px;
+  vertical-align: middle;
+  .me-switch__core {
+    margin: 0;
+    display: inline-block;
+    position: relative;
+    width: 40px;
+    height: 20px;
+    border: 1px solid #dcdfe6;
+    outline: none;
+    border-radius: 10px;
+    box-sizing: border-box;
+    background: #dcdfe6;
+    cursor: pointer;
+    transition: border-color 0.3s, background-color 0.3s;
+    vertical-align: middle;
+    .me-switch__button {
+      position: absolute;
+      top: 1px;
+      left: 1px;
+      border-radius: 100%;
+      transition: all 0.3s;
+      width: 16px;
+      height: 16px;
+      background-color: #fff;
+    }
+  }
+}
 </style>
